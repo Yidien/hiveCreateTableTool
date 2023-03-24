7a6d301cd97448658fa71cc2c4ce977505d61716
Yidien <yidien@qq.com>
2023-03-24 21:25:58 +0800

    1. 之前更新识别from错误的修改，依然会引起其他错误，本次引入正则，重新修正上次错误；
    2. 暂时支持with as语句，需试用，测试功能

--- node.py
+++ node.py
@@ -1,16 +1,52 @@
+import re
+
+
 class Node:
     def __init__(self, name, text):
         self.sheet_name = name
         self.item_list = []
         self.child_node_dict = {}
         self.child_node_list = []
+        self.with_node_dict = {}
         self.raw_text = text
         self.text = self.del_comment_text(text)
+        self.raw_code_text = self.split_with_code_text(self.text)
+        self.code_text = self.merger_with_code_text(self.raw_code_text)
         return
+
+    def split_with_code_text(self, text):
+        start = self.find_string(r'\bwith\b', text)
+        if start == -1:
+            return text
+        text = text[start+4:]
+        while True:
+            name = re.search(r'\b\w*\b', text).group()
+            start, end = self.find_parentheses(text)
+            self.with_node_dict[name] = text[start:end+1]
+            text = text[end+1:]
+            if not re.match(r'\s*,', text):
+                break
+        self.merger_with_code_text(text)
+        return text
+
+    def merger_with_code_text(self, text):
+        key_set = ('where', 'limit', 'group', 'union', 'left', 'right', 'full', 'inner', 'join')
+        code_text = ''.join([(c) for c in text]).lower()
+        for key, value in self.with_node_dict.items():
+            find_pos = re.search(r'\b'+key+r'\b', code_text)
+            if find_pos is None:
+                continue
+            start_pos = find_pos.span(0)[0]
+            end_pos = find_pos.span(0)[0]
+            next_word = re.search(r'\b\w*\b', code_text[find_pos.span(0)[1]+1:])
+            if next_word is None or next_word.group() in key_set:
+                end_pos = find_pos.span(0)[1]
+                value += ' as ' + key + ' '
+            code_text = code_text[:start_pos] + value + code_text[end_pos:]
+        return code_text
 
     @staticmethod
     def del_comment_text(text):
-        start = 0
         ret_list = ''
         while True:
             start = text.find('--')
@@ -53,15 +89,23 @@ class Node:
         ret_list.append(text[start+1:])
         return ret_list
 
+    @staticmethod
+    def find_string(pattern, string):
+        ret = re.search(pattern, string.lower())
+        if ret is None:
+            return -1
+        return ret.span(0)[0]
+
     def get_item(self):
-        start = self.text.lower().find('select') + 6
-        end = self.text.lower().find(' from ')
+        start = self.find_string(r'\bselect\b', self.code_text)
+        end = self.find_string(r'\bfrom\b', self.code_text)
         if start == -1 or end == -1:
             return -1
-        start_distinct = self.text[start:].lower().lstrip().find('distinct')
+        start += 6
+        start_distinct = self.code_text[start:].lower().lstrip().find('distinct')
         if start_distinct == 0:
-            start = self.text.lower().find('distinct') + 8
-        word_list = list(map(lambda x: x.strip(), self.split_comma(self.text[start:end])))
+            start = self.code_text.lower().find('distinct') + 8
+        word_list = list(map(lambda x: x.strip(), self.split_comma(self.code_text[start:end])))
 
         for word in word_list:
             append_list = list((None, None))
@@ -84,10 +128,9 @@ class Node:
                     ret_list.append(item[1])
         return ret_list if ret_list else None
 
-    @staticmethod
-    def find_child_node(text):
-        start_from = text.lower().find(' from ')
-        start_join = text.lower().find(' join ')
+    def find_child_node(self, text):
+        start_from = self.find_string(r'\bfrom\b', text)
+        start_join = self.find_string(r'\bjoin\b', text)
         if start_from == -1 and start_join == -1:
             return None
         if start_from == -1 or (start_join != -1 and start_join < start_from):
@@ -123,7 +166,7 @@ class Node:
             start = self.find_child_node(text)
             if start is None:
                 break
-            text = text[start:]
+            text = text[start+4:]
             start, end = self.find_parentheses(text)
             if end == -1:
                 continue
@@ -142,7 +185,7 @@ class Node:
         analyse_child_list = self.get_analyse_child_list()
         if analyse_child_list is None:
             return
-        self.analyse_child_node(self.text[end:], analyse_child_list)
+        self.analyse_child_node(self.code_text[end:], analyse_child_list)
         return
 
     def get_word(self):
